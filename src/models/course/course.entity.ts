a48b162a59a7318f84199d8b0e4dbc1dde186f1c
AbdulRahman-Sharief <abdulrahmansharief2@gmail.com>
2024-04-11 06:13:44 +0200

    initially implemented course && courseData schemas

--- src/models/course/course.entity.ts
+++ src/models/course/course.entity.ts
@@ -1,8 +1,68 @@
-import { Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { ReviewEntity } from '../review/review.entity';
+import { CourseDataEntity } from './courseData.entity';
 
 export type CourseDocument = HydratedDocument<CourseEntity>;
 @Schema({ timestamps: true, versionKey: false })
-export class CourseEntity {}
+export class CourseEntity {
+  @Prop({ type: String, required: true })
+  name: string;
+
+  @Prop({ type: String, required: true })
+  description: string;
+
+  @Prop({ type: Number, required: true })
+  price: number;
+
+  @Prop({ type: Number })
+  estimatedPrice: number;
+
+  @Prop({
+    type: {
+      _id: false,
+      public_id: { type: String, required: true },
+      url: { type: String, required: true },
+    },
+    required: true,
+  })
+  thumbnail: object;
+
+  @Prop({ type: String, required: true })
+  tags: string;
+
+  @Prop({ type: String, required: true })
+  level: string;
+
+  @Prop({ type: String, required: true })
+  demo_url: string;
+
+  @Prop({ type: [{ _id: false, title: String }] })
+  benefits: object[];
+
+  @Prop({ type: [{ _id: false, title: String }] })
+  prerequisites: object[];
+  @Prop({
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }],
+    required: true,
+  })
+  reviews: ReviewEntity[];
+  @Prop({
+    type: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'CourseData',
+      },
+    ],
+    required: true,
+  })
+  course_data: CourseDataEntity[];
+
+  @Prop({ type: Number, required: true, default: 0 })
+  ratings: number;
+
+  @Prop({ type: Number, required: true, default: 0 })
+  purchased: number;
+}
 
 export const CourseSchema = SchemaFactory.createForClass(CourseEntity);
--- src/models/course/courseData.entity.ts
+++ src/models/course/courseData.entity.ts
@@ -1,8 +1,47 @@
-import { Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument, mongo } from 'mongoose';
+import { QuestionEntity } from '../question/question.entity';
 
 export type CourseDataDocument = HydratedDocument<CourseDataEntity>;
 @Schema({ timestamps: true, versionKey: false })
-export class CourseDataEntity {}
+export class CourseDataEntity {
+  @Prop({ type: String, required: true })
+  title: string;
+
+  @Prop({ type: String, required: true })
+  description: string;
+
+  @Prop({ type: String, required: true })
+  video_url: string;
+
+  @Prop({ type: Object, required: true })
+  video_thumbnail: object;
+
+  @Prop({ type: String, required: true })
+  video_section: string;
+
+  @Prop({ type: Number, required: true })
+  video_length: number;
+
+  @Prop({ type: String, required: true })
+  video_player: string;
+
+  @Prop({
+    type: [
+      {
+        _id: false,
+        title: { type: String, required: true, default: null },
+        url: { type: String, required: true, default: null },
+      },
+    ],
+  })
+  links: object[];
+
+  @Prop({ type: String })
+  suggestion: string;
+
+  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Question' }] })
+  questions: QuestionEntity[];
+}
 
 export const CourseDataSchema = SchemaFactory.createForClass(CourseDataEntity);
